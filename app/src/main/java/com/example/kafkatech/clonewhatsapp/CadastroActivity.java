fe3f11c207138c3ff690434256de59aca3f2719a
alexsander kafka <alexsanderkafka2001alex@gmail.com>
2023-08-14 13:56:52 -0300

    Activity cadastro criada

--- app/src/main/java/com/example/kafkatech/clonewhatsapp/CadastroActivity.java
+++ app/src/main/java/com/example/kafkatech/clonewhatsapp/CadastroActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class CadastroActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
