9b4fae14f4ee6ee3e9003859036eb514a2edbb36
alexsander kafka <alexsanderkafka2001alex@gmail.com>
2023-08-15 14:44:36 -0300

    Adicionado uma toolbar na Activity main

--- app/src/main/java/com/example/kafkatech/clonewhatsapp/activity/MainActivity.java
+++ app/src/main/java/com/example/kafkatech/clonewhatsapp/activity/MainActivity.java
@@ -1,20 +1,57 @@
 package com.example.kafkatech.clonewhatsapp.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.kafkatech.clonewhatsapp.R;
+import com.example.kafkatech.clonewhatsapp.config.ConfiguraFirebase;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = ConfiguraFirebase.getFirebaseAuth();
+
+        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
+        toolbar.setTitle("WhatsApp Fake");
+        setSupportActionBar(toolbar);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.menuSair){
+            deslogarUser();
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void deslogarUser(){
+        try {
+            auth.signOut();
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
