d8b764420671cc59835002c18eb8360ab4155988
alexsander kafka <alexsanderkafka2001alex@gmail.com>
2023-08-21 13:34:18 -0300

    Permissões

--- app/src/main/java/com/example/kafkatech/clonewhatsapp/activity/ConfiguracaoActivity.java
+++ app/src/main/java/com/example/kafkatech/clonewhatsapp/activity/ConfiguracaoActivity.java
@@ -1,15 +1,27 @@
 package com.example.kafkatech.clonewhatsapp.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.example.kafkatech.clonewhatsapp.R;
+import com.example.kafkatech.clonewhatsapp.helper.Permissao;
 
 import java.util.Objects;
 
 public class ConfiguracaoActivity extends AppCompatActivity {
+
+    private final String[] permissoesNecessarias = new String[]{
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +32,40 @@ public class ConfiguracaoActivity extends AppCompatActivity {
         toolbar.setTitle("Configuração");
         setSupportActionBar(toolbar);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        //Validar permissões
+        Permissao.validarPermissoes(permissoesNecessarias, this, 1);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        for(int permissaoResultado : grantResults){
+            if(permissaoResultado == PackageManager.PERMISSION_DENIED){
+                alertaValidacaoPermissao();
+            }
+        }
+
+    }
+
+    private void alertaValidacaoPermissao(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissões Negadas");
+        builder.setMessage("Para utilizar o app é necessário aceitar as permissões");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                abrirTela();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void abrirTela(){
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
     }
 }
---
+++ app/src/main/java/com/example/kafkatech/clonewhatsapp/helper/Permissao.java
@@ -0,0 +1,61 @@
+package com.example.kafkatech.clonewhatsapp.helper;
+
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Permissao {
+
+    public static boolean validarPermissoes(String[] permissoes, Activity activity, int requestCode){
+
+        /*
+        if(Build.VERSION.SDK_INT >= 23){
+            List<String> listaPermissoes = new ArrayList<>();
+
+            //Percorre as permissões passsadas verificando uma a uma
+            //se já yem a permissao liberada
+
+            for(String permissao : permissoes){
+                Boolean temPermissao = ContextCompat.checkSelfPermission(activity, permissao) == PackageManager.PERMISSION_GRANTED;
+                if(!temPermissao) listaPermissoes.add(permissao);
+            }
+
+            if(listaPermissoes.isEmpty()) return true;
+            String[] novasPermissoes = new String[listaPermissoes.size()];
+            listaPermissoes.toArray(novasPermissoes);
+            ActivityCompat.requestPermissions(activity, novasPermissoes, requestCode);
+
+        }
+        return true;*/
+        if (Build.VERSION.SDK_INT >= 23 ){
+
+            List<String> listaPermissoes = new ArrayList<>();
+
+            //Percorre as permissões passadas,
+            //verificando uma a uma
+            //se já tem a permissao liberada
+            for ( String permissao : permissoes ){
+                Boolean temPermissao = ContextCompat.checkSelfPermission(activity, permissao) == PackageManager.PERMISSION_GRANTED;
+                if(!temPermissao) listaPermissoes.add(permissao);
+            }
+
+            //Caso a lista esteja vazia, não é necessário solicitar permissão
+            if(listaPermissoes.isEmpty()) return true;
+            String[] novasPermissoes = new String[listaPermissoes.size()];
+            listaPermissoes.toArray(novasPermissoes);
+
+            //Solicita permissão
+            ActivityCompat.requestPermissions(activity, novasPermissoes, requestCode );
+
+
+        }
+
+        return true;
+    }
+}
