57c9749c523025924132e4cbae2578602872aeb1
alexsander kafka <alexsanderkafka2001alex@gmail.com>
2023-08-29 14:37:14 -0300

    Finalizado a activity de conversa

--- app/src/main/java/com/example/kafkatech/clonewhatsapp/activity/ChatActivity.java
+++ app/src/main/java/com/example/kafkatech/clonewhatsapp/activity/ChatActivity.java
@@ -6,35 +6,31 @@ import androidx.appcompat.widget.Toolbar;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.kafkatech.clonewhatsapp.R;
 import com.example.kafkatech.clonewhatsapp.databinding.ActivityMainBinding;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class ChatActivity extends AppCompatActivity {
 
     private FloatingActionButton fab;
-
+    private CircleImageView imageView;
+    private TextView nomeDoAmigo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
-        Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
-        toolbar.setTitle("Chat");
+        imageView = findViewById(R.id.circleImageFoto);
+        nomeDoAmigo = findViewById(R.id.textViewNomeChat);
+
+        Toolbar toolbar = findViewById(R.id.toolbarChat);
+        setTitle("");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        //Floating action button
-        fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Snackbar.make(v, "Isso é um floating Action Button", Snackbar.LENGTH_SHORT)
-                        .setAction("Action", null).show();
-            }
-        });
-
     }
 }
